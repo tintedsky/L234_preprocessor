83988c9a032185fde2b0b036a910ca3bee211e1e
tintedsky <hongbonew@gmail.com>
2022-11-25 23:46:48 -0500

    Initial Commit

---
+++ L234_preprocessor/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  L234_preprocessor
+//
+//  Created by Hongbo Niu on 2022-11-25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
